462339af9174184a598a4f237f34c898c0734351
bhaskar2414 <bhaskar2414@hotmail.com>
2025-03-15 16:59:04 +0530

    change : add card icon to button

--- eCommerce-nuxt/components/Header.vue
+++ eCommerce-nuxt/components/Header.vue
@@ -71,9 +71,25 @@
           <NuxtLink to="/team" class="text-gray-600 hover:text-gray-800"
             >Team</NuxtLink
           >
-          <NuxtLink to="/cart" class="relative flex items-center space-x-2">
-            <i class="fas fa-shopping-cart text-gray-600"></i>
-            <span class="bg-red-500 text-white rounded-full px-2 py-1 text-xs">
+          <NuxtLink to="/cart" class="relative flex items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6 text-gray-600"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+              />
+            </svg>
+            <span
+              v-if="cartCount"
+              class="absolute -top-2 -right-2 bg-red-500 text-white rounded-full px-2 py-1 text-xs"
+            >
               {{ cartCount }}
             </span>
           </NuxtLink>
