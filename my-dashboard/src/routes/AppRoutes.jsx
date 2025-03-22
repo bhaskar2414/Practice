05452346504fe577f786d712246b02d4095ba2d0
bhaskar2414 <bhaskar2414@hotmail.com>
2025-03-22 23:41:29 +0530

    Feat : add protectedRoute

--- my-dashboard/src/routes/AppRoutes.jsx
+++ my-dashboard/src/routes/AppRoutes.jsx
@@ -1,14 +1,30 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "@pages/Login";
 import Signup from "@pages/Signup";
-import Home from "@pages/Home";
+import Dashboard from "@pages/Dashboard";
+
+const ProtedtedRoutes = ({ children }) => {
+  const isAuthenticated = localStorage.getItem("isAuthenticated") === "true";
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
+  return children;
+};
 
 export default function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
+
+      <Route
+        path="/"
+        element={
+          <ProtedtedRoutes>
+            <Dashboard />
+          </ProtedtedRoutes>
+        }
+      />
     </Routes>
   );
 }
